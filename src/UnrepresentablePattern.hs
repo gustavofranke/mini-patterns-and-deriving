49ba7476ea0eb3c746c9564d9b4af59a8a559642
Gustavo Franke <franke.gustavo@gmail.com>
2021-01-05 12:20:39 +0000

    improve up to three data type

--- src/UnrepresentablePattern.hs
+++ src/UnrepresentablePattern.hs
@@ -13,21 +13,20 @@ group = undefined
 -- Make illegal states unrepresentable: Task 2
 -- Improve the following code by applying the make illegal states unrepresentable pattern.
 
--- this data type can be further simplified based on the fact that we can handle wrapping 1, 2, and 3 elems rather than wrapping the list
 data XValueList a
-  = Length1List [a]
-  | Length2List [a]
-  | Length3List [a]
+  = Length1List a
+  | Length2List a a
+  | Length3List a a a
 
 mkXValueList :: [a] -> Either String (XValueList a)
 mkXValueList ls = case length ls of
   0 -> Left "Empty list"
-  1 -> Right $ Length1List ls
-  2 -> Right $ Length2List ls
-  3 -> Right $ Length3List ls
+  1 -> Right $ Length1List (head ls)
+  2 -> Right $ Length2List (head ls) (ls !! 1)
+  3 -> Right $ Length3List (head ls) (ls !! 1) (ls !! 2)
   _ -> Left "More than three values"
 
 sumUpToThree :: Num a => XValueList a -> a
-sumUpToThree (Length1List [x]) = x
-sumUpToThree (Length2List [x, y]) = x + y
-sumUpToThree (Length3List [x, y, z]) = x + y + z
+sumUpToThree (Length1List x) = x
+sumUpToThree (Length2List x y) = x + y
+sumUpToThree (Length3List x y z) = x + y + z
