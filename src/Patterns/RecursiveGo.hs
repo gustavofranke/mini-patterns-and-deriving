6d60055d50a029893b2c8bb469396f097805d15e
Gustavo Franke <franke.gustavo@gmail.com>
2021-01-08 19:48:36 +0000

    add recursive go tasks

--- src/Patterns/RecursiveGo.hs
+++ src/Patterns/RecursiveGo.hs
@@ -4,11 +4,27 @@ module Patterns.RecursiveGo where
 -- Improve the following code by applying the Recursive go pattern.
 sum0 :: Num a => [a] -> a
 sum0 [] = 0
-sum0 (x:xs) = x + sum0 xs
+sum0 (x : xs) = x + sum0 xs
 
--- Recursive go: Task 2
--- Write the ordNub function with the Recursive go pattern. The function should return all unique elements from a list in the order of their appearance.
-ordNub :: Ord a => [a] -> [a]
-ordNub = undefined 
+-- |
+-- >>> sum0 [1..5]
+-- 15
+-- >>> sum1 [1..5]
+-- 15
+sum1 :: Num a => [a] -> a
+sum1 = go 0
+  where
+    go acc [] = acc
+    go acc (x : xs) = go (acc + x) xs
+
+-- | Recursive go: Task 2
+-- Write the ordNub function with the Recursive go pattern.
+-- The function should return all unique elements from a list in the order of their appearance.
+--
 -- >>> ordNub [3, 3, 3, 2, 2, -1, 1]
 -- [3,2,-1,1]
+ordNub :: Ord a => [a] -> [a]
+ordNub = go []
+  where
+    go res [] = res
+    go res (x : xs) = if x `elem` res then go res xs else go (res ++ [x]) xs
