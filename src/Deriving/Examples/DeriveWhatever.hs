0b24b64fb7c9b9215a3d98b5a3f48dee75e17b99
Gustavo Franke <franke.gustavo@gmail.com>
2021-01-10 14:38:28 +0000

    dump derived functor instance

--- src/Deriving/Examples/DeriveWhatever.hs
+++ src/Deriving/Examples/DeriveWhatever.hs
@@ -1,3 +1,12 @@
+{-# OPTIONS_GHC
+    -ddump-deriv
+    -dsuppress-idinfo
+    -dsuppress-coercions
+    -dsuppress-type-applications
+    -dsuppress-uniques
+    -dsuppress-module-prefixes
+#-}
+
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DerivingStrategies #-}
 
@@ -7,3 +16,16 @@ data Gift a
    = None
    | Some a
    deriving stock (Functor)
+
+-- $ stack ghc src/Deriving/Examples/DeriveWhatever.hs 
+-- [1 of 1] Compiling Deriving.Examples.DeriveWhatever ( src/Deriving/Examples/DeriveWhatever.hs, src/Deriving/Examples/DeriveWhatever.o )
+-- 
+-- ==================== Derived instances ====================
+-- Derived class instances:
+--   instance Functor Gift where
+--     fmap f None = None
+--     fmap f (Some a1) = Some (f a1)
+--     (<$) z None = None
+--     (<$) z (Some a1) = Some ((\ b1 -> z) a1)
+--
+-- Derived type family instances:
