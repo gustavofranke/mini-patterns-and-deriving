62efcc34d96a4534171d27a66c06841ab99f3c34
Gustavo Franke <franke.gustavo@gmail.com>
2021-01-11 17:31:10 +0000

    add usage example of anyclass

--- src/Deriving/Examples/AnyClassDerivations.hs
+++ src/Deriving/Examples/AnyClassDerivations.hs
@@ -1,10 +1,16 @@
+{-# LANGUAGE DeriveAnyClass #-}
+{-# LANGUAGE DerivingStrategies #-}
 {-# LANGUAGE DefaultSignatures #-}
 
 module Deriving.Examples.AnyClassDerivations where
 
+import Data.Text
 class MyEq a where
     equal :: a -> a -> Bool
 
     default equal :: Show a => a -> a -> Bool
     equal x y = show x == show y
 
+data Gift = Gift Text Int
+    deriving stock (Show)
+    deriving anyclass (MyEq)
